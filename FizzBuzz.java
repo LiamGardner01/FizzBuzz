34ea6d807c84fc2beb01ec4fdab32fed50e89a8d
Liam Gardner <gardner.liam01@gmail.com>
2019-03-07 13:47:42 +1100

    Final Solution

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,22 +1,25 @@
-public static void main (String[args])
+public class FizzBuzz
 {
-	for(int i = 1; i <= 100; i++)
+	public static void main (String[] args)
 	{
-		if(i%15 == 0)
+		for(int i = 1; i <= 100; i++)
 		{
-			System.out.println(FizzBuzz);
-		}
-		else if(i%5 == 0)
-		{
-			System.out.println(Buzz);
-		}
-		else if(i%3 == 0)
-		{
-			System.out.println(Fizz);
-		}
-		else
-		{
-			System.out.println(i);
+			if(i%15 == 0)
+			{
+				System.out.println("FizzBuzz");
+			}
+			else if(i%5 == 0)
+			{
+				System.out.println("Buzz");
+			}
+			else if(i%3 == 0)
+			{
+				System.out.println("Fizz");
+			}
+			else
+			{
+				System.out.println(i);
+			}
 		}
 	}
 }
